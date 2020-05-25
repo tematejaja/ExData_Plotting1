6ddff214b15a83b88a483d87d3f03a6384ccf188
tematejaja <64222750+tematejaja@users.noreply.github.com>
2020-05-25 11:24:13 -0500

    Update plot2.R

--- plot2.R
+++ plot2.R
@@ -1,6 +1,12 @@
-data_full <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?", 
+data_full <- read.csv("C:/Users/HERNANDO ALVAREZ/Documents/tarea de chimba", header=T, sep=';', na.strings="?", 
                       nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='\"')
 data1 <- subset(data_full, Date %in% c("1/2/2007","2/2/2007"))
 data1$Date <- as.Date(data1$Date, format="%d/%m/%Y")
-hist(data1$Global_active_power, main="Global Active Power", 
-     xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
+datetime <- paste(as.Date(data1$Date), data1$Time)
+data1$Datetime <- as.POSIXct(datetime)
+dev.copy(png, file="plot2.png", height=480, width=480)
+with(data1, {
+  plot(Global_active_power~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+})
+dev.off()
