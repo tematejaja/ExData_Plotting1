f24fb4f8f06658a87ea4045341548418ec5e2a69
tematejaja <64222750+tematejaja@users.noreply.github.com>
2020-05-25 11:25:11 -0500

    Create plot3.R

---
+++ plot3.R
@@ -0,0 +1,10 @@
+dev.copy(png, file="plot3.png", height=480, width=480)
+with(data1, {
+  plot(Sub_metering_1~Datetime, type="l",
+       ylab="Global Active Power (kilowatts)", xlab="")
+  lines(Sub_metering_2~Datetime,col='Red')
+  lines(Sub_metering_3~Datetime,col='Blue')
+})
+legend("topright", col=c("black", "red", "blue"), lty=1, lwd=2, 
+       legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+dev.off()
